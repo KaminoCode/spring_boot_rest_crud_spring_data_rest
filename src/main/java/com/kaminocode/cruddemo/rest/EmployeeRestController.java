637b60ef0a687ac511576a1b6b8d6743d29c13e0
ozark-dev <jason@ozarkdev.com>
2023-04-17 22:43:08 -0500

    Add the EmployeeRestController within the rest package.

--- src/main/java/com/kaminocode/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/kaminocode/cruddemo/rest/EmployeeRestController.java
@@ -1,4 +1,27 @@
 package com.kaminocode.cruddemo.rest;
 
+import com.kaminocode.cruddemo.dao.EmployeeDAO;
+import com.kaminocode.cruddemo.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    // quick and dirty: inject employee dao
+    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    // expose /employees endpoint and return list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
 }
