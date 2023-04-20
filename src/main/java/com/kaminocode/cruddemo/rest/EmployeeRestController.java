48fee34a004760048dab501cd5809caa3a1f9b30
ozark-dev <jason@ozarkdev.com>
2023-04-19 22:31:22 -0500

    Added method deleteById to EmployeeRestController

--- src/main/java/com/kaminocode/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/kaminocode/cruddemo/rest/EmployeeRestController.java
@@ -61,4 +61,23 @@ public class EmployeeRestController {
         return dbEmployee;
     }
 
+    // add mapping for DELETE /employees/{employeeId} - delete employee
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        // find employee by ID
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " +  employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+
+    }
+
 }
