ac879c87bf9dc130c9a81d5fa7374db90d7affda
ozark-dev <jason@ozarkdev.com>
2023-04-19 22:12:38 -0500

    Add the mapping to EmployeeRestController to add new employee

--- src/main/java/com/kaminocode/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/kaminocode/cruddemo/rest/EmployeeRestController.java
@@ -3,10 +3,7 @@ package com.kaminocode.cruddemo.rest;
 import com.kaminocode.cruddemo.entity.Employee;
 import com.kaminocode.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,5 +36,19 @@ public class EmployeeRestController {
         }
 
         return theEmployee;
+    }
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+        // set id to 0 in case an id is passed in JSON
+        theEmployee.setId(0);
+
+        // save the employee to the database
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        // return dbEmployee since it has the updated id from the database
+        return dbEmployee;
     }
 }
